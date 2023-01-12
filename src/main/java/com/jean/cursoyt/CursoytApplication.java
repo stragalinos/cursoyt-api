4adcb946e18550bdd4d3e2037b67f1a970b4a3a3
stragalinos <jeanneto11.jn@gmail.com>
2023-01-12 10:52:53 -0300

    commit inicial

---
+++ src/main/java/com/jean/cursoyt/CursoytApplication.java
@@ -0,0 +1,13 @@
+package com.jean.cursoyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoytApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jean/cursoyt/CursoytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.cursoyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoytApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
